a628f50d3a274af75adf6bf7d5efd3492d3efd0f
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-08-05 21:45:24 +0200

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(import.meta.dirname, "dis"),
+        clean: true,
+    },
+    devtool: "eval-source-map",
+    devServer: {
+        watchFiles: ["./src/template.html"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            }
+        ],
+    },
+};
