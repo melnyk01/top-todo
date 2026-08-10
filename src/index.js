20355adc5d903876c3789cc8ec88bd63a5a0aad4
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-08-10 21:09:32 +0200

    Improved localStorage logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import "./style.css";
 import { displayList } from "./UI";
-const taskList = JSON.parse(localStorage.getItem("list"));
+
+const taskList = JSON.parse(localStorage.getItem("list")) || [];
+
 displayList(taskList);
 
 const createTaskBtn = document.querySelector('#create-task');
 createTaskBtn.addEventListener('click', () => {
     taskList.push(prompt('Name?!'));
-    displayList(taskList);
     localStorage.setItem("list", JSON.stringify(taskList))
+    displayList(taskList);
 
 })
